e41fe9e3126b60474a8f47a61b4d32c0052d5ac3
Hendrik Mans <hendrik@mans.de>
2022-05-28 14:55:24 +0200

    Revert "Add turbolinks"
    This reverts commit 12df6bb842e24a32d983c6a3eb7647f33e61b461.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
-  integrations: [react(), turbolinks()],
+  integrations: [react()],
 });
