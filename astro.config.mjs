12df6bb842e24a32d983c6a3eb7647f33e61b461
Hendrik Mans <hendrik@mans.de>
2022-05-28 14:39:43 +0200

    Add turbolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
-  integrations: [react()],
+  integrations: [react(), turbolinks()],
 });
